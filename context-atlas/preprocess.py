3d62512dbd5487f36e08819dd42eb8ddddf5e34c
ycho <ycho@utexas.edu>
2022-10-06 11:46:02 -0500

    preprocessing adapted to fit COCA-fiction scene data

--- context-atlas/preprocess.py
+++ context-atlas/preprocess.py
@@ -23,28 +23,28 @@ from __future__ import print_function
 import os
 import torch
 from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
-import sqlite3 as sql
-import re
 import numpy as np
 import umap
 import json
 from tqdm import tqdm
 import nltk
+from nltk.stem import WordNetLemmatizer
 
-
-DB_PATH = './enwiki-20170820.db'
+# DB_PATH = './enwiki-20170820.db'
+COCA_PATH = '/Users/ycho/Music/Dropbox/5_GIT/coca-scene/results'
 nltk.download('averaged_perceptron_tagger')
 nltk.download('punkt')
+lemmatizer = WordNetLemmatizer()
 
-def neighbors(word, sentences):
+def neighbors(word, sentences, include_context=True):
   """Get the info and (umap-projected) embeddings about a word."""
   # Get part of speech of this word.
   sent_data = get_poses(word, sentences)
 
   # Get embeddings.
-  points = get_embeddings(word.lower(), sentences)
+  points = get_embeddings(word.lower(), sentences, include_context)
 
-  # Use UMAP to project down to 3 dimnsions.
+  # Use UMAP to project down to 3 dimensions.
   points_transformed = project_umap(points)
 
   return {'labels': sent_data, 'data': points_transformed}
@@ -57,15 +57,49 @@ def project_umap(points):
     points_transformed.append(transformed)
   return points_transformed
 
-def get_embeddings(word, sentences):
+def get_embeddings(word, sentences, include_context=True):
   """Get the embedding for a word in each sentence."""
   # Tokenized input
   layers = range(-12, 0)
   points = [[] for layer in layers]
   print('Getting embeddings for %d sentences '%len(sentences))
   for sentence in sentences:
+    # Apply lowercase to each sentence
+    sentence = sentence.lower()
+
+    # If include_context is set as False, only take the middle sentence (= the target sentence)
+    if not include_context:
+      sentence = sentence.split('|')[1]  # discarding the prev and next sentences
+    else:
+      tokenized_text_with_sentbound = tokenizer.tokenize(sentence)
+      sentence = sentence.replace(" | ", " ")  # remove sentence boundaries ('|')
+
     sentence = '[CLS] ' + sentence + ' [SEP]'
     tokenized_text = tokenizer.tokenize(sentence)
+
+    # Truncate text to match length limit of BERT encoder
+    if len(tokenized_text) > 512:
+        tokenized_text = tokenized_text[:511] + ['[SEP]']  # makes 512, after adding the last special token [SEP]
+
+    # Find word index in tokenized sentence
+    try:
+      # *** Changed to lemma search from token search (Yejin)
+      # To find indices of lexical variants of a word, search after lemmatization
+      # (e.g., plural form of a noun, different tenses of a verb)
+      lemmatized_tokens = [lemmatizer.lemmatize(w) for w in tokenized_text]
+
+      if not include_context:
+        word_idx = lemmatized_tokens.index(word)
+      else:
+        # To get word index matched in the target sentence (and not in the previous context sentence)
+        word_idx = [i for i, x in enumerate(lemmatized_tokens)
+                    if x == word and i >= tokenized_text_with_sentbound.index("|")][0]
+
+    # If the word is made up of multiple tokens, just use the first one of the tokens that make it up.
+    except:
+      for i, token in enumerate(tokenized_text):
+        if token == word[:len(token)]:
+          word_idx = i
 
     # Convert token to vocabulary indices
     indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
@@ -91,60 +125,28 @@ def get_embeddings(word, sentences):
 
     # We have a hidden states for each of the 12 layers in model bert-base-uncased
     encoded_layers = [l.numpy() for l in encoded_layers]
-    try:
-      word_idx = tokenized_text.index(word)
-    # If the word is made up of multiple tokens, just use the first one of the tokens that make it up.
-    except:
-      for i, token in enumerate(tokenized_text):
-        if token == word[:len(token)]:
-          word_idx = i
 
     # Reconfigure to have an array of layer: embeddings
     for l in layers:
-      sentence_embedding = encoded_layers[l][0][word_idx]
+      try:
+        sentence_embedding = encoded_layers[l][0][word_idx]
+      except IndexError:
+        continue
       points[l].append(sentence_embedding)
 
   points = np.asarray(points)
   return points
 
-def tokenize_sentences(text):
-  """Simple tokenizer."""
-  print('starting tokenization')
+def get_sentences_COCA(word):
+  """Returns a bunch of sentences from COCA-fiction"""
+  print('Selecting sentences from COCA-fiction...')
+  word_fpath = COCA_PATH + '/' + word + '.txt'
+  with open(word_fpath) as f:
+      word_sentences = f.readlines()
 
-  text = re.sub('\n', ' ', text)
-  sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
-
-  # Filter out too long sentences.
-  sentences = [t for t in sentences if len(t) < 150]
-
-  return sentences
-
-
-def get_query(select, db=DB_PATH):
-  """Executes a select statement and returns results and column/field names."""
-  with sql.connect(db) as conn:
-    c = conn.cursor()
-    c.execute(select)
-    col_names = [str(name[0]).lower() for name in c.description]
-  return c.fetchall(), col_names
-
-
-def get_sentences():
-  """Returns a bunch of sentences from wikipedia"""
-  print('Selecting sentences from wikipedia...')
-
-  select = 'select * from articles limit 5000000'
-  docs, _ = get_query(select)
-  docs = [doc[3] for doc in docs]
-  doc = ' '.join(docs)
-  print('Number of articles selected: %d'%len(docs))
-
-  sentences = tokenize_sentences(doc)
-  print('Total number of sentences: %d'%len(sentences))
-  np.random.shuffle(sentences)
-  return sentences
-
-
+  print('Total number of sentences: %d'%len(word_sentences))
+  np.random.shuffle(word_sentences)
+  return word_sentences
 
 def get_poses(word, sentences):
   """Get the part of speech tag for the given word in a list of sentences."""
@@ -152,8 +154,20 @@ def get_poses(word, sentences):
   for sent in sentences:
     text = nltk.word_tokenize(sent)
     pos = nltk.pos_tag(text)
+    lemmatized = [lemmatizer.lemmatize(w) for w in text]
+
     try:
-      word_idx = text.index(word)
+      # word_idx = text.index(word)
+      # Get word index in the target sentence (after a sentence boundary '|')
+      # ------------------------------------------------------------------------------------
+      # NB. Each mini document in COCA-fiction-scene data comprises of three sentences,
+      #     which are one target sentence (that includes the query word)
+      #           and two context sentences (one before and one after the target sentence):
+      #
+      #         "<previous sentence>|<target sentence>|<next sentence>"
+      #
+      # ------------------------------------------------------------------------------------
+      word_idx = [i for i, x in enumerate(lemmatized) if x == word and i > text.index("|")][0]
       pos_tag = pos[word_idx][1]
     except:
       pos_tag = 'X'
@@ -166,12 +180,15 @@ def get_poses(word, sentences):
 
 
 if __name__ == '__main__':
+  # Whether to include neighboring (prev and next) sentences as context when embedding a word
+  include_context = False
 
   device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
   print("device : ", device)
 
   # Load pre-trained model tokenizer (vocabulary)
   tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
   # Load pre-trained model (weights)
   model = BertModel.from_pretrained('bert-base-uncased')
   model.eval()
@@ -181,28 +198,26 @@ if __name__ == '__main__':
   with open('static/words.json') as f:
     words = json.load(f)
 
-  sentences = get_sentences()
-
   for word in tqdm(words):
-    # Filter out sentences that don't have the word.
-    sentences_w_word = [t for t in sentences if ' ' + word + ' ' in t]
+    word_sentences = get_sentences_COCA(word)
 
-    # Take at most 200 sentences.
-    sentences_w_word = sentences_w_word[:1000]
+    # Take at most 1000 sentences.
+    sentences_w_word = word_sentences[:1000]
 
-    # And don't show anything if there are less than 100 sentences.
-    if (len(sentences_w_word) > 100):
-      print('starting process for word : %s'%word)
-      locs_and_data = neighbors(word, sentences_w_word)
-      with open('static/jsons/%s.json'%word, 'w') as outfile:
-        json.dump(locs_and_data, outfile)
+    # Process words and dump embeddings as json
+    print(f'starting process for word : {word}')
+    locs_and_data = neighbors(word, sentences_w_word, include_context)
+    with open(f'static/jsons/{word}.json', 'w') as outfile:
+      json.dump(locs_and_data, outfile)
 
   # Store an updated json with the filtered words.
   filtered_words = []
   for word in os.listdir('static/jsons'):
-    word = word.split('.')[0]
-    filtered_words.append(word)
+    if word.endswith('.json'):
+      word = word.split('.')[0]
+      filtered_words.append(word)
 
   with open('static/filtered_words.json', 'w') as outfile:
     json.dump(filtered_words, outfile)
   print(filtered_words)
+
