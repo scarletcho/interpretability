7307ba03ab8ca909ab7bbd74788f77afb5931e36
Yejin Cho <ycho@utexas.edu>
2023-10-23 13:06:32 -0500

    Added data inspection code

--- context-atlas/bert-visualize.py
+++ context-atlas/bert-visualize.py
@@ -9,20 +9,29 @@ import textwrap
 import re
 import sys
 from sklearn.metrics import pairwise_distances_argmin
+
 pio.renderers.default = 'browser'
 
+# Pandas dataframe setting + print texts
+pd.set_option('display.max_colwidth', -1)  # for the full display of text
 
 def get_neighbors(indices, query_id):
     query_id_mapped = dfid_to_intid[query_id]
     top10_neighbor_ids = indices[query_id_mapped][1:]  # The first nearest neighbor is the vector itself
     # return top10_neighbor_ids
     print(df.loc[query_id, 'sent'])
-    print(df.loc[top10_neighbor_ids, "sent"].to_string())
+    print(df.loc[top10_neighbor_ids, ["uqid", "sent"]])
+    # df.loc[top10_neighbor_ids, ["uqid", "sent"]].to_csv(word+'-'+str(layer_number)+'.csv')
+    # df.to_csv('your_array.csv', header=False, index=False)
+    # print(df.loc[top10_neighbor_ids, "sent"].to_string())
+
 
 # Query word and layer number to look at
-# word = 'coffee'
+# word = 'crow'
 word = sys.argv[1]
-layer_number = 8
+
+# layer_number = 7
+layer_number = int(sys.argv[2])
 
 # Load json data
 with open('static/jsons/' + word + '.json') as f:
@@ -44,11 +53,11 @@ df['idx-print'] = df.index
 df.index = df.index.astype(int)
 
 # Apply some formatting to sent for better visibility
-df["sent-print"] = df["sent"].apply(lambda txt: re.sub("(["+word[0].upper()+word[0].lower()+"])"+word[1:],
-                                                       r"<b>\1"+word[1:]+r"</b>", txt))
+df["sent-print"] = df["sent"].apply(lambda txt: re.sub("([" + word[0].upper() + word[0].lower() + "])" + word[1:],
+                                                       r"<b>\1" + word[1:] + r"</b>", txt))
 df["sent-print"] = df["sent-print"].apply(lambda txt: '<br>'.join(textwrap.wrap(txt, width=40)))
 fig = px.scatter(df, x="x", y="y", color="cluster", category_orders={"cluster": np.sort(df['cluster'].unique())},
-                 hover_data={"x":False, "y":False, "cluster": False, "sent-print": True, "idx-print": True},
+                 hover_data={"x": False, "y": False, "cluster": False, "sent-print": True, "idx-print": True},
                  color_discrete_sequence=px.colors.qualitative.Light24)
 
 fig.update_layout(
@@ -59,19 +68,17 @@ fig.update_layout(
 
 fig.update_layout(
     title={
-        'text': word,
-        'y':0.95,
-        'x':0.5,
+        'text': word + "-" + str(layer_number),
+        'y': 0.95,
+        'x': 0.5,
         'xanchor': 'center',
         'yanchor': 'top'})
 
 fig.update_traces(hovertemplate="%{customdata[1]}<br>ID:%{customdata[2]}")
 fig.show()
-fig.write_image("./plot/" + word + ".png")
-fig.write_html("./plot/" + word + ".html")
+fig.write_image("./plot/" + word + "-" + str(layer_number) + ".png")
+fig.write_html("./plot/" + word + "-" + str(layer_number) + ".html")
 
-# Pandas dataframe setting + print texts
-pd.set_option('display.max_colwidth', -1)  # for the full display of text
 # print(df['sent'].to_string())
 
 # Get neighbors of a specific sentence
@@ -80,16 +87,24 @@ nbrs = NearestNeighbors(n_neighbors=11).fit(X)
 distances, indices = nbrs.kneighbors(X)
 dfid_to_intid = dict(zip(df.index, range(len(X))))
 intid_to_dfid = dict(zip(range(len(X)), df.index))
-indices_mapped = [list(map(intid_to_dfid.get, indices[i])) for i in range(len(indices))]
+uqid_to_dfid = dict(zip(df.uqid, df.index))
+uqid_to_intid = dict(zip(df.uqid, zip(range(len(X)))))
 
+indices_mapped = [list(map(intid_to_dfid.get, indices[i])) for i in range(len(indices))]
 
 # Inspect data
 
 # Centroids
 centroids = kmeans.cluster_centers_
 nearest_to_centroids = pairwise_distances_argmin(centroids, X, metric='euclidean')
+
+# Aggregate neighboring sentences (used for BERT embedding)
+df['3sents'] = df['prev'] + ' ' + df['sent'] + ' ' + df['next']
+
 df_centroids = df.iloc[nearest_to_centroids]
-# print(df_centroids['sent'])
+print('Centroids:')
+print(df_centroids[['uqid', '3sents']])
+
 for cent_id in range(len(df_centroids)):
     print('Centroid#:' + str(cent_id))
     cent_row = df_centroids.iloc[cent_id]
@@ -97,8 +112,7 @@ for cent_id in range(len(df_centroids)):
     get_neighbors(indices_mapped, cent_query_id)
 
 
+# Inspection of each data
 # query_id = 138
 # get_neighbors(indices_mapped, query_id)
-
-
-
+# get_neighbors(indices_mapped, uqid_to_dfid[2689088])
